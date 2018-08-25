ae2a164ad7eb036e53cd7bfdb3223caeb3091850
Jason Viger <jason.viger@gmail.com>
2018-08-25 09:29:26 +0000

    automatically backed up by learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,12 +1,7 @@
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   
   def change
-    create_table :shows do |t|
-      t.string   :name
-      t.string   :network
-      t.string   :day 
-      t.integer  :rating
-    end
+    add_column :shows, :season, :string
   end
   
   
