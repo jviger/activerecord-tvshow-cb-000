38ed12d19e7f1176aad38e668f2947994168964b
Jason Viger <jason.viger@gmail.com>
2018-08-25 09:27:57 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -5,7 +5,7 @@ class CreateShows < ActiveRecord::Migration[5.2]
       t.string   :name
       t.string   :network
       t.string   :day 
-      t.integer   :rating
+      t.integer  :rating
     end
   end
   
