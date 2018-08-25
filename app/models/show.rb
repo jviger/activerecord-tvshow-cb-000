a7ebee405909e23c985873e0072bb6b3f29a9651
Jason Viger <jason.viger@gmail.com>
2018-08-25 09:42:35 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,27 +1,4 @@
 class Show < ActiveRecord::Base 
   
-  def highest_rating
-    Show.maximum(:rating)
-  end
-  
-  def most_popular_show
-    Show.find_by(rating: highest_rating)
-  end
-  
-  def lowest_rating
-    Show.minimum(:rating)
-  end
-  
-  def least_popular_show
-    Show.find_by(rating: lowest_rating)
-  end
-  
-  def popular_shows
-    Show.where("rating > 5")
-  end
-  
-  def shows_by_alphabetical_order
-    Show.order(name: :asc)
-  end
   
 end
